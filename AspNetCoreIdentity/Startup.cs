9308bde2cb3e5e63e49f867087aa9fb0a33b28cb
Bryan <bryanlima200@outlook.com>
2022-04-06 23:52:47 -0300

    Copiado configuração de IdentityHostingStartup para Startup e adaptado conforme necessidade e alterado servidor local no appsettings

--- AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/Startup.cs
@@ -1,6 +1,9 @@
+using AspNetCoreIdentity.Areas.Identity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,12 @@ namespace AspNetCoreIdentity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AspNetCoreIdentityContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddDefaultUI()
+                    .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
             services.AddControllersWithViews();
         }
 
@@ -45,6 +54,8 @@ namespace AspNetCoreIdentity
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
